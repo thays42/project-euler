b5812bc0a0d6ba41d45bedc6a84e1e71398cb34e
Tyler Hays <tyhays@gmail.com>
2017-06-14 19:04:24 -0700

    Remove _ thousands separator

--- python/problem1.py
+++ python/problem1.py
@@ -5,7 +5,7 @@ def main():
 
     Find the sum of all the multiples of 3 or 5 below 1000.
     '''
-    return sum([x for x in range(1_000) if x % 3 == 0 or x % 5 == 0])
+    return sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
 
 if __name__ == '__main__':
     print(main())
